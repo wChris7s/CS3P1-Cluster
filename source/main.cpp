8cf71be8e7f900af4ba6a30d4a61ce4b5027a2a7
wChris7s <cw.aranibar.solaligue@gmail.com>
2024-09-21 21:25:21 -0500

    feat: cmake

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,24 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+    const std::string default_message = "@%s: Hello from process [ %i ] out of [ %i ] processes.\n";
 
-	std::string default_message = "@%s: Hello from process [ %i ] out of [ %i ] processes.\n";
+    MPI_Init(&argc, &argv);
 
-	MPI_Init(&argc, &argv);
-  
-	int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
-	if (world_rank == 0) {
-		printf(default_message.c_str(), processor_name, world_rank, world_size);
-	} else {
     printf(default_message.c_str(), processor_name, world_rank, world_size);
-	}
 
-  MPI_Finalize();
-  return 0;
+    MPI_Finalize();
+    return 0;
 }
